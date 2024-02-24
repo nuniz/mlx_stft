afafc2f1566aee1229b8c12fd751134a58cfad10
zorea <zoreasaf@gmail.com>
2024-02-24 14:13:54 +0200

    changed amp_to_db to nn class

--- mlx_stft/utils.py
+++ mlx_stft/utils.py
@@ -1,22 +1,29 @@
 import mlx.core as mx
+import mlx.nn as nn
 
-def amp_to_db(
-    x: mx.array, eps:float=1e-6, top_db:float=40
-) -> mx.array:
-    """
-    Convert the input tensor from amplitude to decibel scale.
+class AmpToDB(nn.Module):
+    def __init__(self, eps: float = 1e-5, top_db: float = 80.0):
+        """
+        Initializes the AmpToDB module.
 
-    Arguments:
-        x {[mx.array]} -- [Input tensor.]
+        Arguments:
+            eps {float} -- Small value to avoid numerical instability. (default: 1e-5)
+            top_db {float} -- Threshold the output at ``top_db`` below the peak (default: 80.0)
+        """
+        super().__init__()
+        self.eps = eps
+        self.top_db = top_db
 
-    Keyword Arguments:
-        eps {[float]} -- [Small value to avoid numerical instability.]
-                          (default: {1e-6})
-        top_db {[float]} -- [threshold the output at ``top_db`` below the peak]
-            `             (default: {40})
+    def __call__(self, x: mx.array) -> mx.array:
+        """
+        Forward pass of the AmpToDB module.
 
-    Returns:
-        [mx.array] -- [Output tensor in decibel scale.]
-    """
-    x_db = 20 * mx.log10(x.abs() + eps)
-    return mx.max(x_db, (x_db.max(-1).values - top_db).unsqueeze(-1))
+        Arguments:
+            x {mx.array} -- Input tensor.
+
+        Returns:
+            mx.array -- Output tensor in dB scale.
+        """
+        x_db = 20 * mx.log10(x.abs() + self.eps)
+        max_vals = x_db.max(-1).values
+        return mx.max(x_db, (max_vals - self.top_db).unsqueeze(-1))
