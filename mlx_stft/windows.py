8756ade4ba5453e77a9e0cd5615a9d3cb1a61f01
zorea <zoreasaf@gmail.com>
2024-02-27 00:18:13 +0200

    updated windows, removed gaussian window

--- mlx_stft/windows.py
+++ mlx_stft/windows.py
@@ -1,11 +1,11 @@
 import mlx.core as mx
 
 
-def get_window(window, length, **kwargs) -> mx.array:
+def get_window(window, length) -> mx.array:
     """Generate a window of a specified type.
 
     Args:
-        window (str): Type of window. Supported types are 'rectangular', 'hann', 'gaussian', 'blackman'.
+        window (str): Type of window. Supported types are 'rectangular', 'hann', 'blackman'.
         length (int): Length of the window.
         **kwargs: Additional arguments specific to the chosen window type.
 
@@ -13,16 +13,14 @@ def get_window(window, length, **kwargs) -> mx.array:
         mx.array: Window of specified type.
     """
     if window == "rectangular":
-        return rect_window(length, **kwargs)
+        return rect_window(length)
     elif window == "hann":
-        return hann_window(length, **kwargs)
-    elif window == "gaussian":
-        return gaussian_window(length, **kwargs)
+        return hann_window(length)
     elif window == "blackman":
-        return blackman_window(length, **kwargs)
+        return blackman_window(length)
     else:
         raise ValueError(
-            "Unsupported window type. Supported types are 'rectangular', 'hann', 'gaussian', 'blackman'."
+            "Unsupported window type. Supported types are 'rectangular', 'hann','blackman'."
         )
 
 
@@ -48,20 +46,6 @@ def rect_window(length: int) -> mx.array:
         mx.array: Rectangular window of specified length.
     """
     return mx.ones(length)
-
-
-def gaussian_window(length: int, sigma: float = 1) -> mx.array:
-    """Generate a Gaussian window.
-
-    Args:
-        length (int): Length of the window.
-        sigma (float): Standard deviation of the Gaussian distribution.
-
-    Returns:
-        mx.array: Gaussian window of specified length.
-    """
-    n = mx.arange(length)
-    return mx.exp(-0.5 * ((n - (length - 1) / 2) / sigma) ** 2)
 
 
 def blackman_window(length: int) -> mx.array:
